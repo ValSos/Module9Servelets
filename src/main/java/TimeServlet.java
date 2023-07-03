c175bb010ab5a76d2db416c2a14abe63b08a2681
Valeriia Sosiedka <lera.dovgan.96@gmail.com>
2023-07-03 12:39:56 +0300

    Initial commit

---
+++ src/main/java/TimeServlet.java
@@ -0,0 +1,77 @@
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.context.Context;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@WebServlet(value = "/time-template")
+public class TimeServlet extends HttpServlet {
+    private TemplateEngine engine;
+
+    @Override
+    public void init() throws ServletException {
+        engine = new TemplateEngine();
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(engine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+        engine.addTemplateResolver(resolver);
+    }
+
+    private static String getCurrentTime(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+
+        String timezone = req.getParameter("timezone");
+
+        if (timezone == null) {
+            timezone = getLastTimeZone(req);
+        } else {
+            timezone = timezone.replace(" ", "+");
+            resp.addCookie(new Cookie("lastTimezone", timezone));
+        }
+
+        ZoneId zoneId = ZoneId.of(timezone);
+        ZonedDateTime now = ZonedDateTime.now(zoneId);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+        String formattedTime = now.format(formatter);
+
+        return formattedTime;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+
+        Context context = new Context(req.getLocale(), Map.of("time", getCurrentTime(req, resp)));
+        engine.process("time-template", context, resp.getWriter());
+        resp.getWriter().close();
+
+    }
+
+    public static String getLastTimeZone(HttpServletRequest req){
+        String result = "UTC";
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("lastTimezone")) {
+                    result = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+
+}
---
+++ src/main/java/TimezoneValidateFilter.java
@@ -0,0 +1,42 @@
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.ZoneId;
+
+/*
+@WebFilter(value = "/time/*")
+public class TimezoneValidateFilter extends HttpFilter {
+    @Override
+    public void doFilter(HttpServletRequest req,
+                         HttpServletResponse resp,
+                         FilterChain chain) throws IOException, ServletException {
+        resp.setContentType("text/html; charset=utf-8");
+        String authHeaderValue = req.getHeader("timezone");
+        String timezone = req.getParameter("timezone");
+        if (timezone == null) {
+            chain.doFilter(req, resp);
+        } else if (isValidTimezone(timezone.replace(" ", "+"))) {
+            chain.doFilter(req, resp);
+        } else {
+            resp.setContentType("text/html; charset=utf-8");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write("Invalid timezone");
+            return;
+        }
+
+    }
+
+    public static boolean isValidTimezone(String timezone) {
+        try {
+            ZoneId.of(timezone);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
+*/
